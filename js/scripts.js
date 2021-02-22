11858781ee7657c15aac022b8f2a85114d3b8f4b
JokaMilen <77240807+JokaMilen@users.noreply.github.com>
2021-02-22 12:24:04 +0100

    made pokemon list and added objects to the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-alert('Hello world');
-let favoriteFood = 'soup';
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList[0] = {name: "Bulbasaur", height: 0.7, types: ['grass','poison']};
+pokemonList[1] = {name: "Ivysaur", height: 1, types: ['grass','poison']};
+pokemonList[2] = {name: "Charmander", height: 0.6, types: ['fire']};
