883b31c62adc6800cf1c7270a678fa0aa5495f57
JokaMilen <77240807+JokaMilen@users.noreply.github.com>
2021-02-26 10:20:04 +0100

    js syntax change for adding objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 let pokemonList = [];
-pokemonList[0] = {name: "Bulbasaur", height: 0.7, types: ['grass','poison']};
-pokemonList[1] = {name: "Ivysaur", height: 1, types: ['grass','poison']};
-pokemonList[2] = {name: "Charmander", height: 0.6, types: ['fire']};
+pokemonList.push({name: "Bulbasaur", height: 0.7, types: ['grass','poison']});
+pokemonList.push({name: "Ivysaur", height: 1, types: ['grass','poison']});
+pokemonList.push({name: "Charmander", height: 0.6, types: ['fire']});
