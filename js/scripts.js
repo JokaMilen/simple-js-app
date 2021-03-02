d8fac20cda50e64e971a235cc2ba6770aa7eec96
JokaMilen <77240807+JokaMilen@users.noreply.github.com>
2021-03-02 13:15:10 +0100

    iterated pokemon list and printed list on webpage

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,11 @@ let pokemonList = [];
 pokemonList.push({name: "Bulbasaur", height: 0.7, types: ['grass','poison']});
 pokemonList.push({name: "Ivysaur", height: 1, types: ['grass','poison']});
 pokemonList.push({name: "Charmander", height: 0.6, types: ['fire']});
+
+document.write("<ul>")
+for (let i = 0; i<pokemonList.length; i++) {
+  let currentPokemon = pokemonList[i];
+  let bigText = currentPokemon.height >= 1 ? " - Wow, that’s big!" : "";
+  document.write("<li>" + currentPokemon.name + " (height: "+ currentPokemon.height +")" + bigText + "</li>");
+}
+document.write("</ul>")
