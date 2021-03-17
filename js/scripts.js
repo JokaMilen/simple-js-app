02d0605412178fb77cefca49c2a1ae5b685badb4
JokaMilen <77240807+JokaMilen@users.noreply.github.com>
2021-03-17 18:51:37 +0100

    code update from for to forEach loop and put pokemonList into IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,33 @@
-let pokemonList = [];
-pokemonList.push({name: "Bulbasaur", height: 0.7, types: ['grass','poison']});
-pokemonList.push({name: "Ivysaur", height: 1, types: ['grass','poison']});
-pokemonList.push({name: "Charmander", height: 0.6, types: ['fire']});
+let pokemonRepository = (
+  function() {
+    let pokemonList = [];
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
 
-document.write("<p>")
-for (let i = 0; i<pokemonList.length; i++) {
-  let currentPokemon = pokemonList[i];
-  let bigText = currentPokemon.height >= 1 ? " - Wow, that’s big!" : "";
-  document.write(currentPokemon.name + " (height: "+ currentPokemon.height +")" + bigText + "<br>");
+    function getAll() {
+      return pokemonList;
+    }
+
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
+
+
+
+pokemonRepository.add({name: "Bulbasaur", height: 0.7, types: ['grass','poison']});
+pokemonRepository.add({name: "Ivysaur", height: 1, types: ['grass','poison']});
+pokemonRepository.add({name: "Charmander", height: 0.6, types: ['fire']});
+
+function printPokemon(pokemon) {
+  let bigText = pokemon.height >= 1 ? " - Wow, that’s big!" : "";
+  document.write(pokemon.name + " (height: "+ pokemon.height +")" + bigText + "<br>");
 }
-document.write("</p>")
+
+let pokemonList = pokemonRepository.getAll();
+
+document.write("<p>");
+pokemonList.forEach(printPokemon);
+document.write("</p>");
