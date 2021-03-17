39249365d20c5b49a369c8e10506e4081ea32c3c
JokaMilen <77240807+JokaMilen@users.noreply.github.com>
2021-03-17 17:37:18 +0100

    tag adjustment in js for pokemontlist

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@ pokemonList.push({name: "Bulbasaur", height: 0.7, types: ['grass','poison']});
 pokemonList.push({name: "Ivysaur", height: 1, types: ['grass','poison']});
 pokemonList.push({name: "Charmander", height: 0.6, types: ['fire']});
 
-document.write("<ul>")
+document.write("<p>")
 for (let i = 0; i<pokemonList.length; i++) {
   let currentPokemon = pokemonList[i];
   let bigText = currentPokemon.height >= 1 ? " - Wow, that’s big!" : "";
-  document.write("<li>" + currentPokemon.name + " (height: "+ currentPokemon.height +")" + bigText + "</li>");
+  document.write(currentPokemon.name + " (height: "+ currentPokemon.height +")" + bigText + "<br>");
 }
-document.write("</ul>")
+document.write("</p>")
