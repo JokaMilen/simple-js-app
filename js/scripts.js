5ddffc6ca859b3b87b5a9629f71b16f041f4686b
JokaMilen <77240807+JokaMilen@users.noreply.github.com>
2021-03-18 20:32:45 +0100

    created pokemon list with buttons, added event listener for each button, added some button and list styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 let pokemonRepository = (
   function() {
     let pokemonList = [];
+
     function add(pokemon) {
       pokemonList.push(pokemon);
     }
@@ -9,25 +10,36 @@ let pokemonRepository = (
       return pokemonList;
     }
 
+    function addListItem(pokemon) {
+      let list = document.querySelector(".pokemon-list");
+      let listItem = document.createElement("li");
+      let button = document.createElement("button");
+      button.innerText = pokemon.name;
+      button.classList.add("pokemon-button");
+      button.addEventListener('click', function (event) {
+        showDetails(pokemon);
+      });
+
+      listItem.appendChild(button);
+      list.appendChild(listItem);
+    }
+
+    function showDetails(pokemon) {
+      console.log(pokemon);
+    }
+
     return {
       add: add,
-      getAll: getAll
+      getAll: getAll,
+      addListItem: addListItem
     };
   })();
-
-
 
 pokemonRepository.add({name: "Bulbasaur", height: 0.7, types: ['grass','poison']});
 pokemonRepository.add({name: "Ivysaur", height: 1, types: ['grass','poison']});
 pokemonRepository.add({name: "Charmander", height: 0.6, types: ['fire']});
 
-function printPokemon(pokemon) {
-  let bigText = pokemon.height >= 1 ? " - Wow, that’s big!" : "";
-  document.write(pokemon.name + " (height: "+ pokemon.height +")" + bigText + "<br>");
-}
 
-let pokemonList = pokemonRepository.getAll();
-
-document.write("<p>");
-pokemonList.forEach(printPokemon);
-document.write("</p>");
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
